5d9743700b4d973fb916faf4e8ab3177d55ef390
vg1522002gmailcom <vg1522002@gmail.com>
2022-10-10 20:05:25 +0530

    add one problem

---
+++ problem15.js
@@ -0,0 +1,12 @@
+let reg_email="abc@gmail"
+let reg_password="1234"
+
+let ent_email="abc@gmail"
+let ent_password="1234"
+if(reg_email==ent_email){
+  console.log("login")
+}
+  if(reg_password==ent_password){
+    console.log("somthing wrong")
+}
+

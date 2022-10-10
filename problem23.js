34d6af5238d3a2c8b4add8a33243c0b10a870fdc
vg1522002gmailcom <vg1522002@gmail.com>
2022-10-10 20:33:13 +0530

    add another

---
+++ problem23.js
@@ -0,0 +1,28 @@
+
+let day = "fri"
+
+switch (day) {
+  case "sun": console.log("sunday")
+    break;
+  case "mon": console.log("monday")
+    break;
+  case "tue": console.log("tuesday")
+    break;
+  case "wed": console.log("wednesday")
+    break;
+    
+  case "thus": console.log("thusday")
+    break;
+  case "fri": console.log("friday")
+    break;
+  case "sat": console.log("saterday")
+
+}
+
+
+
+
+
+
+
+
